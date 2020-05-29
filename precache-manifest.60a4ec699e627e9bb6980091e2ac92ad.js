ffe432982f7f3f4973e1d5e86bcfc9916d41ddab
REZA <hamidmick@yahoo.com>
2020-05-28 22:07:29 -0400

    Updates

---
+++ precache-manifest.60a4ec699e627e9bb6980091e2ac92ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb529a2350a0f73268cd16ca2418e22",
+    "url": "/myRobotFriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/myRobotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a674cdbead9e983fdbc6",
+    "url": "/myRobotFriends/static/css/main.06a473d1.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/myRobotFriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myRobotFriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a674cdbead9e983fdbc6",
+    "url": "/myRobotFriends/static/js/main.d700a0ed.chunk.js"
+  },
+  {
+    "revision": "a2756f1f5c4ecb07749a",
+    "url": "/myRobotFriends/static/js/runtime-main.56520d66.js"
+  },
+  {
+    "revision": "0272879d9c0658afdeed1fdcc140d653",
+    "url": "/myRobotFriends/static/media/Generator REX.0272879d.woff"
+  }
+]);
